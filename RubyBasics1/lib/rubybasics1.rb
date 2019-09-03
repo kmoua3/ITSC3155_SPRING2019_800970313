5a1a499a60927262e6f93b7ab0780205fc0a8f15
Ku Moua <kmoua3@uncc.edu>
2019-09-03 15:56:26 +0000

    RubyBasics1 – Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -1,17 +1,15 @@
 # Lab 1
 # Part I
 def sum arr
-  # YOUR CODE HERE
   return arr.sum
 end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
   return arr.max(2).sum
 end
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any?{|a,b|a+b == n}
 end
