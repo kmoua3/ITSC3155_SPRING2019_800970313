0200ecd53e43b366df709d8a92438f5ca0abab35
Ku Moua <kmoua3@uncc.edu>
2019-09-24 16:42:49 +0000

    Ruby OO Basics – Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,11 +1,12 @@
 # Ruby Basics Part 3
 
 class BookInStock
-attr_accessor :isbn, :price
-def initialize isbn, price
+    attr_accessor :isbn, :price
+    def initialize isbn, price
     
-@isbn = isbn
-@price = price
-
-end
+        @isbn = isbn
+        @price = price
+        
+        raise ArgumentError if @isbn.empty? || @price <= 0
+    end
 end
