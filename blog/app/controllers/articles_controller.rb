40cf769409139055a884cd5bfe8f4bec69559e90
Ku Moua <kmoua3@uncc.edu>
2019-10-13 20:47:56 +0000

    Complete Create and Show.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,8 +1,24 @@
 class ArticlesController < ApplicationController
+    
+    
     def new
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        
+        @article.save
+        redirect_to @article
+        
+        def show
+       @article = Article.find(params[:id]) 
+        end
     end
 end
+
+private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+    
+    
